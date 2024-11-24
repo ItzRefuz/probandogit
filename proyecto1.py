53ab201fc56cb3504c4993d5cf90ea26ba5e588b
ItzRefuz <bryan.muniz18@gmail.com>
2024-11-23 20:50:14 -0400

    Se agregaron los comentarios en proyecto1.py

--- proyecto1.py
+++ proyecto1.py
@@ -4,3 +4,5 @@ filename = input('Entre el nombre')
 filename = open(filename)
 
 print('Esta es la ultima version')
+
+#aqui vamos a poner un comentario como si hubieramos documentado todo el documento
